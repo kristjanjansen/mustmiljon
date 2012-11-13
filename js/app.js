e216f75d2082912b6a4992397009ff491171cefc
Kristjan Jansen <kristjan.jansen@gmail.com>
2012-11-13 19:17:52 +0200

    fixes

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ var ds = new Miso.Dataset({
   importer : Miso.Dataset.Importers.GoogleSpreadsheet,
   parser : Miso.Dataset.Parsers.GoogleSpreadsheet,
   key : "0AjuiYlqIySuFdEV5cHdJdW5hbkhJdFdMM19LZTd1TEE",
-//  key : "0Asnl0xYK7V16dFpFVmZUUy1taXdFbUJGdGtVdFBXbFE",
   worksheet : "1",
   fast : true
 });
@@ -13,14 +12,16 @@ ds.fetch({
   success : function() {
 
     ds.each(function(row, rowIndex) {
-      rows.push(row);      
+      if (row.disabled !== 'yes') {
+        rows.push(row);
+      }
     });
 
     $('#treemap').treemap(rows, {
       mouseenter: function (node, box) {
-        $('.legend').html('<span class="label">' + node.label + '</span> <span class="value">' + Math.round(node.value / 1000000) + '</span> miljonit €');
+        $('.legend').html('<span class="label">' + node.label + '</span> <span class="value">' + (node.value / 1000000).toFixed(1) + '</span> miljonit €');
       },
-      mmouseleave: function (node, box) {
+      mouseleave: function (node, box) {
         $('.legend').html('');
       }
     });
